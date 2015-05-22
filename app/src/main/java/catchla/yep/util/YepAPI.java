97860f30cb5974fea97348e3c5ab32770e06f59d
Mariotaku Lee <mariotaku.lee@gmail.com>
2015-05-23 03:00:00 +0800

    implementing api

--- app/src/main/java/catchla/yep/util/YepAPI.java
+++ app/src/main/java/catchla/yep/util/YepAPI.java
@@ -1,9 +1,18 @@
 package catchla.yep.util;
 
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+
 /**
  * Created by mariotaku on 15/5/12.
  */
-public class YepAPI {
+public interface YepAPI {
 
+    @POST("/v1/registration/create")
+    @FormUrlEncoded
+    void createRegistration(@Field("mobile") String mobile, @Field("nickname") String nickname,
+                            @Field("phone_code") String phoneCode, @Field("longitude") double longitude,
+                            @Field("latitude") double latitude);
 
 }
