bb182afc9466f7a8068cf68eaff32834e823dfe2
Mariotaku Lee <mariotaku.lee@gmail.com>
2016-04-05 20:09:02 +0800

    updated version

--- src/main/java/catchla/yep/model/util/ISO8601DateConverter.java
+++ src/main/java/catchla/yep/model/util/ISO8601DateConverter.java
@@ -1,12 +1,9 @@
 package catchla.yep.model.util;
 
 import com.bluelinelabs.logansquare.typeconverters.DateTypeConverter;
-import com.fasterxml.jackson.core.JsonParser;
 
-import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 /**
@@ -18,8 +15,4 @@ public class ISO8601DateConverter extends DateTypeConverter {
         return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
     }
 
-    @Override
-    public Date parse(final JsonParser jsonParser) throws IOException {
-        return super.parse(jsonParser);
-    }
 }
