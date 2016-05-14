ee53b0842e8d029dbff449bb1c085028d31ba50d
Mariotaku Lee <mariotaku.lee@gmail.com>
2016-05-14 15:11:08 +0800

    updated chat menu

---
+++ src/main/java/catchla/yep/fragment/ChatMenuBottomSheetDialogFragment.java
@@ -0,0 +1,9 @@
+package catchla.yep.fragment;
+
+import android.support.design.widget.BottomSheetDialogFragment;
+
+/**
+ * Created by mariotaku on 16/5/14.
+ */
+public class ChatMenuBottomSheetDialogFragment extends BottomSheetDialogFragment {
+}
