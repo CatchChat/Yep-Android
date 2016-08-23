bff76da0ea01dcc256af4608590fa00866473e26
Mariotaku Lee <mariotaku.lee@gmail.com>
2016-08-23 17:50:15 +0800

    improved send message insert

--- src/main/kotlin/catchla/yep/fragment/TopicChatListFragment.kt
+++ src/main/kotlin/catchla/yep/fragment/TopicChatListFragment.kt
@@ -7,7 +7,6 @@ import catchla.yep.extension.account
 import catchla.yep.loader.HistoricalMessagesLoader
 import catchla.yep.model.Message
 import catchla.yep.model.Paging
-import java.util.*
 
 /**
  * Created by mariotaku on 15/12/10.
@@ -31,10 +30,19 @@ class TopicChatListFragment : ChatListFragment() {
     }
 
     fun addMessage(result: Message) {
-        val list = adapter.data?.toMutableList() ?: ArrayList()
-        list.add(result)
-        list.sortByDescending { it.createdAt }
-        adapter.data = list
+        val adapterData = adapter.data
+        if (adapterData is MutableList) {
+            val index = adapterData.indexOfFirst { result.createdAt > it.createdAt }
+            adapterData.add(index, result)
+            adapter.notifyItemInserted(index)
+        } else if (adapterData != null) {
+            val index = adapterData.indexOfFirst { result.createdAt > it.createdAt }
+            val list = adapterData.toMutableList()
+            list.add(index, result)
+            adapter.data = list
+        } else {
+            adapter.data = mutableListOf(result)
+        }
     }
 
 }
